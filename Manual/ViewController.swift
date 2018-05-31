a106188c5e657e41189ffb8a793cf751f94b7b3f
Karmatsheten507 <39786975+Karmatsheten507@users.noreply.github.com>
2018-05-31 11:24:37 +0700

    manua

---
+++ Manual/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manual
+//
+//  Created by Nando Septian Husni on 5/22/18.
+//  Copyright © 2018 imastudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
